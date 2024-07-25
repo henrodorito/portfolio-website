64a3c07d6b450c7bd926f6107aaf53f28ccc3a0d
Henry Huang <henhua@amazon.com>
2024-07-24 19:01:49 -0700

    React Portfolio Website Tutorial Setup

---
+++ components/projects.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.5);
+
+  return (
+    <section id="projects" ref={ref} className="scroll-mt-28 mb-28">
+      <SectionHeading>My Projects</SectionHeading>
+
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
